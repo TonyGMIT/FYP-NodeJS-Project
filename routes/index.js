0d3b6814cc15bb9f28373808ff15b23081f74227
Tony <g00372842@gmit.ie>
2022-04-21 04:58:12 +0100

    Final Repo commit for backend

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,21 @@ let router = express.Router();
 let mongoose = require("mongoose");
 const cors=require("cors");
 router.use(cors());
-mongoose.connect("mongodb://127.0.0.1:27017/SmartTabs");
+mongoose.connect("mongodb://127.0.0.1:27017/SmartTabs"); // connection to the database 
 let Schema = mongoose.Schema;
 
 let blogSchema = new Schema(
   {
-    x1: String, y1: String,
+    x1: String, y1: String,// x and y values for the activity chart
     x2: String, y2: String,
     x3: String, y3: String,
     x4: String, y4: String,
     x4: String, y5: String,
     x6: String, y6: String,
     x7: String, y7: String,
+    t1: String, // t value for the gauge total
   },
-  { collection: "MouseTracker", versionKey:false }
+  { collection: "MouseTracker", versionKey:false } // chooosing the correct collection
 );
 let Model = mongoose.model("SmartTabs", blogSchema);
 
@@ -25,16 +26,8 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" })
 });
 
-// router.post("/setNoEmployees", function (req, res, next) {
-//   console.log(req.body)
-//   var data = new EmployeeModel(req.body)
-//   data.save()
-//   res.end()
-// });
-
 router.get("/getData", function (req, res, next) {
   Model.find().then(function (docs) {
-     // theDoc = JSON.stringify(theDoc)
     res.json(docs)
   });
 });
